2fd0f1810125153491f2366ef663e95f7e9a0f1a
Daniel-Gauntlett <dangauntlett@gmail.com>
2024-11-26 20:02:53 -0500

    Basis for routing

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import eventroutes from "./events.js";
+import roomroutes from "./rooms.js";
+import userroutes from "./users.js";
+export const constructorMethod = (app) => {
+    app.use('/events',eventroutes);
+    app.use('/rooms',roomroutes);
+    app.use('/users',userroutes);
+    app.use("*",(req,res) => {
+        return res.status(404).json({error:'Not found'});
+    });
+};
