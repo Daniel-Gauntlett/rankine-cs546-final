95b35c8596aca60ec1df81d40e22ed9e5c5bf6f2
fhaziz073 <fhaziz073@gmail.com>
2024-12-14 13:42:05 -0500

    Added patch function for events collection

--- seed/seed_events.js
+++ seed/seed_events.js
@@ -8,7 +8,7 @@ let event1 = null;
 let event2 = null;
 try
 {
-    event1 = await events.createEvent("General GBM", "Our general GBM, where we discuss a variety a topics each week!",
+    event1 = await events.createEvent("General GBM    ", "Our general GBM, where we discuss a variety a topics each week!",
         new Date("September 10, 2024 20:00:00 EST").toJSON(), new Date("September 10, 2024 21:00:00 EST").toJSON(), true,
         new Date("December 05, 2024 21:00:00 EST").toJSON(), false, "6744b2de7d63b9e12950d9d3", 1,
         "6744b30707e6c26e2ef42c9a", [], ["6744b325694adc1370a267e0"], "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOBFRML09gNsjdrpsbp0TxbSaQ95AOy-fIw&s"
@@ -27,6 +27,7 @@ try
         null, true, "6744b2de7d63b9e12950d9d3", 2,
         "6744b30707e6c26e2ef42c9a", [], ["6744b325694adc1370a267e0"], "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOBFRML09gNsjdrpsbp0TxbSaQ95AOy-fIw&s"
     ));
+    console.log(await events.patchEvent(event2._id, {name: "Party for PSI", isRecurring: false}));
 }
 catch(e)
 {
