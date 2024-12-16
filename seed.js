e2fe8bfbdc09ba4c03e8256607544a624df98e22
Daniel-Gauntlett <dangauntlett@gmail.com>
2024-12-15 21:07:45 -0500

    edit seeded events

--- seed.js
+++ seed.js
@@ -50,17 +50,17 @@ try
     event1 = await events.createEvent("General GBM", "Our general GBM, where we discuss a variety a topics each week!",
         new Date("September 10, 2024 20:00:00 EST").toJSON(), new Date("September 10, 2024 21:00:00 EST").toJSON(), true,
         new Date("December 05, 2024 21:00:00 EST").toJSON(), false, "6744b2de7d63b9e12950d9d3", 1,
-        "6744b30707e6c26e2ef42c9a", [], ["6744b325694adc1370a267e0"], "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOBFRML09gNsjdrpsbp0TxbSaQ95AOy-fIw&s"
+        "dgauntlett", [], ["dgauntlett"], "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOBFRML09gNsjdrpsbp0TxbSaQ95AOy-fIw&s"
     );
     console.log(event1);
     event2 = await events.createEvent("Party For PHI", "Join the Party at our Frat House! Free Food Provided",
         new Date("November 10, 2024 20:00:00 EST").toJSON(), new Date("November 10, 2024 21:00:00 EST").toJSON(), false,
         null, true, "6744b2de7d63b9e12950d9d3", 2,
-        "6744b30707e6c26e2ef42c9a", [], ["6744b325694adc1370a267e0"], "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOBFRML09gNsjdrpsbp0TxbSaQ95AOy-fIw&s"
+        "dbarrand", [], ["dgauntlett"], "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOBFRML09gNsjdrpsbp0TxbSaQ95AOy-fIw&s"
     );
     console.log(event2);
     event3 = await events.createEvent("Boba And Paint!", "Come relax and paint while sipping on boba provided by Kung Fu Tea", 
-        new Date("September 10, 2024 20:00:00 EST").toJSON(), new Date("September 10, 2024 21:00:00 EST").toJSON());
+        new Date("September 10, 2024 20:00:00 EST").toJSON(), new Date("September 10, 2024 21:00:00 EST").toJSON(),false,null,false,"6744b2de7d63b9e12950d9d3",1,"jbrown",[],[],"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOBFRML09gNsjdrpsbp0TxbSaQ95AOy-fIw&s");
     console.log(event3);
     console.log(await events.getAllEvents());
 }
