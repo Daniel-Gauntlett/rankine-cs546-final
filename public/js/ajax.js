6c29525553e66b971d60bd530b7e9b8385bfaae6
Daniel-Gauntlett <dangauntlett@gmail.com>
2024-12-16 02:12:10 -0500

    More ajax

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,9 +1,12 @@
 (function ($) {
-    let makeEventButton = $('#make_event_button');
+    let makeEventButton = $('#confirm_make_event');
     let makeEventForm = $('#make_event_form');
     let eventList = $('#event-list');
-    let recurringCheckbox = $('isRecurringCheckbox');
-    let recurringDateBox = $('recurDateBox');
+    let recurringCheckbox = $('#isRecurringCheckbox');
+    let recurringDateBox = $('#recurDateBox');
+    let approvalForm = $('#approve_form');
+    let approvalCheckbox = $('#approval-checkbox');
+    let eventInfo = $("#eventInfo");
     makeEventButton.submit(function (event) {
         event.preventDefault();
         makeEventButton.hide();
@@ -13,6 +16,20 @@
     recurringCheckbox.click(function (event) {
         recurringDateBox.toggle();
     })
-    
+    approvalForm.submit(function (event) {
+        event.preventDefault();
+        let patch = {};
+        if(approvalCheckbox.prop('checked')){
+            patch.status = 2;
+        } else {
+            patch.status = 0;
+        }
+        let requestConfig = {
+            method: 'PATCH',
+            url: '/events/' + eventInfo.attr("class"),
+            data: patch
+        };
+        $.ajax(requestConfig); 
+    })
 
 })(window.jQuery);
