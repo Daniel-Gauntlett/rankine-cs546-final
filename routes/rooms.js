d8b7cc018c3dc287284e6bcbfad22303782e6cbd
dbarrand06 <dbarrand@stevens.edu>
2024-12-16 22:58:09 -0500

    fixed the thing

--- routes/rooms.js
+++ routes/rooms.js
@@ -38,7 +38,7 @@ router.route('/').get(async (req, res) => {
       await helpers.checkCreateRoom(roomData.roomID,roomData.building,roomData.roomNumber,roomData.roomCapacity,roomData.roomFeatures,roomData.unavailableTimesList,roomData.roomPicture)
       unavailableTimes = calculateUnavailableTimes(unavailableTimesList)
     } catch (e) {
-      return res.status(400).json({error: e});
+      return res.status(400).json({error: "root create room error"});
     }
     try {
         const newRoom = await createRoom(roomData.building,
@@ -59,18 +59,20 @@ router.route('/roomCreate').get(async (req, res) => {
   res.render('roomCreate')
 }).post(async (req, res) => {
   Object.keys(req.body).forEach((c) => req.body[c] = xss(req.body[c]));
+  let unavailableTimesList = [[req.body.startDate, req.body.endDate, req.body.isRecurring, req.body.endRecurring]]
     try {
-      helpers.checkCreateRoom(req.body.building, req.body.roomNumber, req.body.roomCapacity, req.body.roomFeatures, req.body.unavailableTimesList, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png");
+      helpers.checkCreateRoom(req.body.building, req.body.roomNumber, parseInt(req.body.roomCapacity), req.body.roomFeatures.split(',').map(item => item.trim()), unavailableTimesList, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png");
     } catch (e) {
+      console.log("room check error")
       console.log(e);
-      return res.status(400).render('roomCreate',{error: e});
+      return res.status(400).render('roomCreate',{error: "room check error"});
     }
     try {
-      helpers.createRoom(req.body.building, req.body.roomNumber, req.body.roomCapacity, req.body.roomFeatures, req.body.unavailableTimesList, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png");
+      createRoom(req.body.building, req.body.roomNumber, parseInt(req.body.roomCapacity), req.body.roomFeatures.split(',').map(item => item.trim()), unavailableTimesList, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png");
       res.redirect("/");
     } catch (e) {
       console.log(e);
-      res.status(400).render('roomCreate',{error: e});
+      res.status(400).render('roomCreate',{error: "room create error"});
     }
 })
 
